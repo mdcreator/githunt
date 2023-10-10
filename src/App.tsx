60bb70fab01b90a5e3b7032c77933bfc270ccc49
creator.marina <creator.marina@gmail.com>
2023-10-10 23:11:41 +0300

    creating components and pages

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+import { Routes, Route } from "react-router-dom";
+import HomePage from "./pages/HomePage";
+import FavouritePage from "./pages/FavouritePage";
+import Navigation from "./components/Navigation";
+
+function App() {
+  return (
+    <>
+      <Navigation />
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/favourite" element={<FavouritePage />} />
+      </Routes>
+    </>
+  );
+}
+
+export default App;
