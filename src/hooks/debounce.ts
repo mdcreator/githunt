279c62994b599ba2e09cde6614176940a21b3005
creator.marina <creator.marina@gmail.com>
2023-10-13 00:22:53 +0300

    work with requests

---
+++ src/hooks/debounce.ts
@@ -0,0 +1,17 @@
+import React, { useEffect, useState } from "react";
+
+const useDebounce = (value: string, delay = 300): string => {
+  const [debounced, setDebounced] = useState(value);
+
+  useEffect(() => {
+    const handler = setTimeout(() => setDebounced(value), delay);
+
+    return () => {
+      clearTimeout(handler);
+    };
+  }, [value, delay]);
+
+  return debounced;
+};
+
+export default useDebounce;
